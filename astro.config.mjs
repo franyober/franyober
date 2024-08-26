4f8a1b4089fb7b833e6de34e65b8354ecb6bf3b5
franyober <franyober123@hotmail.com>
2024-08-26 01:24:18 -0500

    tercer intento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://franyober.github.io',
+  site: 'https://franyober.github.io/franyober',
   base: 'franyober.github.io',
 })
